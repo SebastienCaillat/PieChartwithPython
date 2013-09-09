314b0ea3f8c48ab3b058bb243222fa33dc3b1006
Sébastien <sebastien.caillat@gmail.com>
2013-09-09 22:47:14 +0200

    Update and rename PieChart to PieChart.py

--- PieChart.py
+++ PieChart.py
@@ -1,7 +1,7 @@
 """ Small program to create pie graphs with or without hatching
 Please note that if hatching selected, values will no apear inside the pie
 one option is to add values in the labels i.e. 'toto \n 10 %'
-Usage: date to be filled in the program then run to generate figure
+Usage: data to be filled in the program then run to generate figure
     Argument after program name will change figure filename
     nofile will avoid saving figure to file
 Modules needed: numpy, matpotlib, pylab... see error message 
